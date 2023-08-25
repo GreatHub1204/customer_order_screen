af6eb329c1c8cb499f39bbff9fec7a0640a191b5
phoonweile <viktor.ivanov0223@gmail.com>
2023-08-25 16:42:46 +0900

    fix the style for show action

--- assets/script/input.js
+++ assets/script/input.js
@@ -1,16 +1,18 @@
 let show_div = document.getElementById('memo_display_show');
 let none_div = document.getElementById('memo_display_none');
-
+let add_display_style = document.querySelector("section.display-info");
 show_div.style.display = "none";
+add_display_style.style.margin = "51px 0";
 
 function show(){
-    show_div.style.display = "contents";
+    show_div.style.display = "inline-flex";
     none_div.style.display = "none";
+    add_display_style.style.margin = "9px 0 0 0";
 }
 
 function run() {
     var dateInput = document.getElementById("delivery").value;
     var dateArray = dateInput.split("-");
-    var formattedDate = dateArray[2] + "/" + dateArray[1] + "/" + dateArray[0];
+    var formattedDate = dateArray[2] + " / " + dateArray[1] + " / " + dateArray[0];
     document.getElementById("srt").value = formattedDate;
 } 
