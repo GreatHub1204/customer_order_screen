927425c86eff422832bb44faad4d686f34c4be81
phoonweile <viktor.ivanov0223@gmail.com>
2023-08-25 12:17:00 +0900

    js run function fix

--- assets/script/input.js
+++ assets/script/input.js
@@ -1,16 +1,4 @@
 
-// n = new Date();
-// y = n.getFullYear();
-// m = n.getMonth() + 1;
-// d = n.getDate();
-// if (m < 9) {
-//     m = "0" + m;
-// }
-// if (d < 9) {
-//     m = "0" + d;
-// }
-// document.getElementById("delivery_date").innerHTML = m + "/" + d + "/" + y;
-// document.getElementById("delivery_date").value = m + "/" + d + "/" + y;
 
 function run() {
     var dateInput = document.getElementById("delivery").value;
@@ -20,11 +8,5 @@ function run() {
     console.log(formattedDate);
 } 
 
-// インプットタグに現在日付をデフォルトで現示
-
-// const currentDate = new Date().toISOString().split("T")[0];
-// let nodeList = document.querySelectorAll("input[type]");
-// nodeList[2].value = currentDate;
-// nodeList[4].value = currentDate;
 
 
