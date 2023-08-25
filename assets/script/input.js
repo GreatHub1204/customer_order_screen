df74c3041f7b1b2abcf3dead4800460592409a30
phoonweile <viktor.ivanov0223@gmail.com>
2023-08-25 13:35:29 +0900

    js modify

--- assets/script/input.js
+++ assets/script/input.js
@@ -8,21 +8,9 @@ function show(){
     none_div.style.display = "none";
 }
 
-
-n = new Date();
-y = n.getFullYear();
-m = n.getMonth() + 1;
-d = n.getDate();
-if (m < 9) {
-    m = "0" + m;
-}
-if (d < 9) {
-    m = "0" + d;
-}
-document.getElementById("delivery_date").innerHTML = m + "/" + d + "/" + y;
-document.getElementById("delivery_date").value = m + "/" + d + "/" + y;
-
 function run() {
-    document.getElementById("srt").value = document.getElementById("selected_date").value;
-    console.log(document.getElementById("srt").value);
+    var dateInput = document.getElementById("delivery").value;
+    var dateArray = dateInput.split("-");
+    var formattedDate = dateArray[2] + "/" + dateArray[1] + "/" + dateArray[0];
+    document.getElementById("srt").value = formattedDate;
 } 
